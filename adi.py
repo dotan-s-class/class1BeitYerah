c5b1435e96fc5d9a70e23891c01483e98e9ebb08
User <aditzur12@gmail.com>
2024-12-18 19:43:12 +0200

    adi2

--- adi.py
+++ adi.py
@@ -1,3 +1,3 @@
 def peleg():
-    print("when i was young i fell into a street ground light and opened my ")
+    print("when i was young i fell into a street ground light and opened my leg")
 peleg()
